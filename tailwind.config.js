7919ab1b2a935962af5f1b8bfdc3a3868ef96493
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2023-02-18 17:06:36 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body:['Nunito']
+      },
+    },
   },
   plugins: [],
 }
